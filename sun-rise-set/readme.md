e5da4bb272f9a8736a04cc778a1ec50a049c031f
morswin22 <xmorswinx@gmail.com>
2018-04-27 21:56:16 +0200

    Added variant selector to sun-rise-set

--- sun-rise-set/readme.md
+++ sun-rise-set/readme.md
@@ -1,4 +1,10 @@
 # Embeding
+Embed dark variant (default):
 ```html
 <iframe src="https://morswin22.github.io/p5-projects/sun-rise-set/embed.html" width="300" height="350" style="border: 0" allow="geolocation"></iframe>
+```
+
+Embed light variant:
+```html
+<iframe src="https://morswin22.github.io/p5-projects/sun-rise-set/embed.html?variant=light" width="300" height="350" style="border: 0" allow="geolocation"></iframe>
 ```
--- sun-rise-set/sketch.js
+++ sun-rise-set/sketch.js
@@ -48,6 +48,10 @@ function setup() {
     frameRate(15);
     getYahoo();
     textFont(Lato);
+    let variant = getParameterByName('variant');
+    if (variant && variant == 'light') {
+        dark = false;
+    }
 }
 
 function draw() {
@@ -142,4 +146,14 @@ function fixFullTime(num) {
         num = "0" + num;
     }
     return num;
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
