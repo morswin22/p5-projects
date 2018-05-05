83f647260d10c726dbf5a5dbed7437fc76544e34
morswin22 <xmorswinx@gmail.com>
2018-05-05 22:42:27 +0200

    Adding water-ripples

---
+++ water-ripples/sketch.js
@@ -0,0 +1,56 @@
+let cols;
+let rows;
+
+let current = [];
+let previous = [];
+
+const dampening = 0.87;
+
+function setup() {
+    createCanvas(850, 550);
+    cols = width;
+    rows = height;
+    for (x = 0; x < cols; x++) {
+        current[x] = [];
+        previous[x] = [];
+        for (y = 0; y < rows; y++) {
+            current[x][y] = 0;
+            previous[x][y] = 0;
+        }
+    }
+}
+
+function draw() {
+    background(0);
+    loadPixels();
+    for (x = 1; x < cols-1; x++) {
+        for (y = 1; y < rows-1; y++) {
+            current[x][y] = (
+                  previous[x-1][y]
+                + previous[x+1][y]
+                + previous[x][y-1]
+                + previous[x][y+1]) / 2 
+                - current[x][y];
+
+            current[x][y] *= dampening;
+
+            pixels[(x*4 + y*4*cols) + 0] = current[x][y] * 255;
+            pixels[(x*4 + y*4*cols) + 1] = current[x][y] * 255;
+            pixels[(x*4 + y*4*cols) + 2] = current[x][y] * 255;
+            pixels[(x*4 + y*4*cols) + 3] = 255;
+        }
+    }
+    updatePixels();
+
+    let temp = previous;
+    previous = current;
+    current = temp;
+
+    if (random(1) < 0.25) {
+        current[parseInt(random(width))][parseInt(random(height))] = 100;
+    }
+}
+
+function mousePressed() {
+    current[mouseX][mouseY] = 100;
+}
